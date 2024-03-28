f9413862b06db5fdb132e7d436bdffa9cb725c25
Shatrudra03 <shatrudrapratapsingh5@gmail.com>
2024-03-29 01:09:38 +0530

    TrailCarouselWidgetfirst

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import { View } from "react-native";
+import TrailCarousel from "./Home/TrailCarousel";
+import CalenderWidget from "./Home/CalenderWidget";
+import MenuItems from "./Home/MenuItems";
 
     export default function HomeScreen() {
         return (
           <View >
-            <TrailCarousel />
-            <CalenderWidget />
-            <MenuItems />
+            <TrailCarousel/>
+            <CalenderWidget/>
+            <MenuItems/>
           </View>
         );
     }

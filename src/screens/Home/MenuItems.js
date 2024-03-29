4a6a11d6d115e04897cffee82e01e108b95c8156
Ayudh Pratap Singh <ayudhpratapsingh@gmail.com>
2024-03-29 07:39:34 -0400

    Navigation pages

--- src/screens/Home/MenuItems.js
+++ src/screens/Home/MenuItems.js
@@ -1,42 +1,47 @@
 import { StyleSheet, View } from "react-native";
 import Card from "./Card";
+import { useNavigation } from '@react-navigation/native';
 
 export default function MenuItems() {
-  const handleCardPress = (routeName) => {};
+  const navigation = useNavigation();
+  const handleCardPress = (routeName) => {
+    navigation.navigate(routeName);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.row}>
         <Card
           title="Trails"
-          onPress={() => handleCardPress("Page1")}
+          onPress={() => handleCardPress("CharitraScreen")}
           imageUrl={require("../../../assets/menuItemsImages/rampath.jpg")}
         />
         <Card
           title="Darshan"
-          onPress={() => handleCardPress("Page2")}
+          onPress={() => handleCardPress("CharitraScreen")}
+          
           imageUrl={require("../../../assets/menuItemsImages/Darshan.jpg")}
         />
         <Card
           title="Teachings"
-          onPress={() => handleCardPress("Page3")}
+          onPress={() => handleCardPress("CharitraScreen")}
           imageUrl={require("../../../assets/menuItemsImages/Teachings.jpg")}
         />
       </View>
       <View style={styles.row}>
         <Card
           title="Danik Pragati"
-          onPress={() => handleCardPress("Page4")}
+          onPress={() => handleCardPress("CharitraScreen")}
           imageUrl={require("../../../assets/menuItemsImages/UpdatedDainikPragato.jpg")}
         />
         <Card
           title="Hindu Calendar"
-          onPress={() => handleCardPress("Page5")}
+          onPress={() => handleCardPress("CharitraScreen")}
           imageUrl={require("../../../assets/menuItemsImages/HinduCalender.jpg")}
         />
         <Card
           title="Feedback"
-          onPress={() => handleCardPress("Page6")}
+          onPress={() => handleCardPress("CharitraScreen")}
           imageUrl={require("../../../assets/menuItemsImages/Screenshot (116).png")}
         />
       </View>

e5696720f594b989ca2691b151833d581356a94a
Vaibhav Rana <vbhvrn@gmail.com>
2024-03-29 07:49:26 +0000

    Add paginations

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,13 +2,14 @@ import { View } from "react-native";
 import TrailCarousel from "./Home/TrailCarousel";
 import CalenderWidget from "./Home/CalenderWidget";
 import MenuItems from "./Home/MenuItems";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-    export default function HomeScreen() {
-        return (
-          <View >
-            <TrailCarousel/>
-            <CalenderWidget/>
-            <MenuItems/>
-          </View>
-        );
-    }
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <TrailCarousel />
+      <CalenderWidget />
+      <MenuItems />
+    </SafeAreaView>
+  );
+}

9959a651401aab3fa41cbcf4da2644df83ad2937
Shatrudra03 <shatrudrapratapsingh5@gmail.com>
2024-04-06 14:23:20 +0530

    NEW

--- src/screens/DarshanScreen/DarshanScreen.js
+++ src/screens/DarshanScreen/DarshanScreen.js
@@ -1,16 +1,16 @@
 import { ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import TempleCarousel from "./TempleCarousel";
-// import ParsadAndOther from "./ParsadAndOther";
-// import HotelsAndRestaurantCarousel from "./HotelsAndRestaurantCarousel";
+import ParsadAndOther from "./ParsadAndOther";
+import HotelsAndRestaurantCarousel from "./HotelsAndRestaurantCarousel";
 
 export default function DarshanScreen() {
   return (
     <SafeAreaView>
       <ScrollView>
         <TempleCarousel />
-        {/* <ParsadAndOther />
-        <HotelsAndRestaurantCarousel /> */}
+        <ParsadAndOther />
+        <HotelsAndRestaurantCarousel />
       </ScrollView>
     </SafeAreaView>
   );

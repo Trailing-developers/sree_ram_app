3b8c1989814ef25fd35103a8955994578e1b69c4
Vaibhav Rana <vbhvrn@gmail.com>
2024-05-22 09:29:10 +0100

    Add bE integeration with app

--- src/screens/Home/CalenderWidget.js
+++ src/screens/Home/CalenderWidget.js
@@ -1,24 +1,43 @@
 import {
+  ActivityIndicator,
   Image,
   StyleSheet,
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
+import { useHomeWidgets } from "../../hooks/api/homewidgets";
+import { useMemo } from "react";
 
 const imageUrl =
   "https://d.newsweek.com/en/full/1978216/dragon-dance-chinese-new-year.jpg";
 
 export default function CalenderWidget() {
+  const { data, isLoading, error } = useHomeWidgets();
   const onPress = () => {
     // Handle banner press event
   };
+
+  const calenderBanner = useMemo(() => {
+    if (!data) return [];
+    return data?.data?.filter((item) => item?.type === "CALENDAR_BANNER")[0]
+      .data[0];
+  }, [data]);
+
+  if (error) return <Text>Something went wrong.</Text>;
+
+  if (isLoading)
+    return (
+      <View style={{ flex: 1, paddingBottom: 10 }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
 
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.container}>
         <Image
-          source={{ uri: imageUrl }}
+          source={{ uri: calenderBanner?.imgUrl }}
           style={styles.image}
           resizeMode="cover"
         />

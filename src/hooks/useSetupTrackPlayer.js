37a4a4b37bb19ea6cf23c777588e75e865b456d9
Vaibhav Rana <vbhvrn@gmail.com>
2024-06-04 20:11:55 +0100

    fix some error on the screen

--- src/hooks/useSetupTrackPlayer.js
+++ src/hooks/useSetupTrackPlayer.js
@@ -2,11 +2,15 @@ import { useEffect, useRef, useState } from "react";
 import TrackPlayer, { RepeatMode } from "react-native-track-player";
 
 const setupPlayer = async () => {
-  await TrackPlayer.setupPlayer({
-    maxCacheSize: 1024 * 10,
-  });
-  await TrackPlayer.setVolume(0.3); // not too loud
-  await TrackPlayer.setRepeatMode(RepeatMode.Queue); // repeat mode
+  try {
+    await TrackPlayer.setupPlayer({
+      maxCacheSize: 1024 * 10,
+    });
+    await TrackPlayer.setVolume(0.3); // not too loud
+    await TrackPlayer.setRepeatMode(RepeatMode.Queue); // repeat mode
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const useSetupTrackPlayer = ({ onLoad }) => {
--- src/screens/DarshanScreen/temple_detail_page/HistogramV2.js
+++ src/screens/DarshanScreen/temple_detail_page/HistogramV2.js
@@ -1,5 +1,7 @@
 import { Dimensions, StyleSheet, View } from "react-native";
 import { BarChart } from "react-native-gifted-charts";
+import { width } from "../TempleCarousel";
+import { useEffect, useState } from "react";
 
 //https://gifted-charts.web.app/barchart
 //more details to use https://github.com/Abhinandan-Kushwaha/react-native-gifted-charts/blob/master/docs/BarChart/BarChartProps.md
@@ -7,6 +9,7 @@ import { BarChart } from "react-native-gifted-charts";
 const screenWidth = Dimensions.get("window").width;
 
 export default function HistogramV2({ dataPoints }) {
+  const [barData, setBarData] = useState([]);
   const chartConfig = {
     backgroundGradientFrom: "#fff",
     backgroundGradientFromOpacity: 0,
@@ -24,18 +27,21 @@ export default function HistogramV2({ dataPoints }) {
     decimalPlaces: 0,
   };
 
-  const barData = dataPoints?.map((item) => {
-    return {
-      value: item?.timing,
-      label: item?.label,
-      frontColor: item?.color,
-    };
-  });
+  useEffect(() => {
+    const dp = dataPoints?.map((item) => {
+      return {
+        value: item?.timing,
+        label: item?.label,
+        frontColor: item?.color,
+      };
+    });
+    setBarData(dp);
+  }, [dataPoints]);
 
   return (
     <View style={styles.container}>
       <BarChart
-        barWidth={20}
+        barWidth={screenWidth / barData?.length}
         noOfSections={2}
         barBorderRadius={4}
         frontColor="lightgray"

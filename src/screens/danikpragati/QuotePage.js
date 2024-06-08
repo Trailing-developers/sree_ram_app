aa8b7faea21ec9e0357bf43cfa16bcc8604bd823
Rashmi Pundir <rashmipundir@Rashmis-MacBook-Air.local>
2024-06-08 11:37:17 +0530

    A new change on Quote page

--- src/screens/danikpragati/QuotePage.js
+++ src/screens/danikpragati/QuotePage.js
@@ -5,80 +5,104 @@ import { SliderBox } from "react-native-image-slider-box";
 const MahabharataImages = [
   "https://spiritualquotes.in/wp-content/uploads/2023/04/4-5-1024x1024.jpg",
   "https://spiritualquotes.in/wp-content/uploads/2023/04/1-12-1024x1024.jpg",
-  "https://spiritualquotes.in/wp-content/uploads/2023/04/16-5-1024x1024.jpg"
+  "https://spiritualquotes.in/wp-content/uploads/2023/04/16-5-1024x1024.jpg",
 ];
-const GeetaImages=[
+const GeetaImages = [
   "https://qph.cf2.quoracdn.net/main-qimg-984397ec72f969ff9e6fb3b20a51fa87-lq",
   "https://i.pinimg.com/736x/f5/d2/e2/f5d2e2d221fca60040496be8ec555523.jpg",
-  "https://www.scrolldroll.com/wp-content/uploads/2019/07/Bhagavad-Gita-Quotes-1.jpg"
+  "https://www.scrolldroll.com/wp-content/uploads/2019/07/Bhagavad-Gita-Quotes-1.jpg",
 ];
-const VedasImages=[
+const VedasImages = [
   "https://spiritualquotes.in/wp-content/uploads/2023/03/50-1024x1024.jpg",
-  "https://ancientscience.in/media/2017/11/Ancient-Science-Quotes-01.jpg"
+  "https://ancientscience.in/media/2017/11/Ancient-Science-Quotes-01.jpg",
 ];
 
 export const QuotePage = () => {
   return (
     <ScrollView style={{ flex: 1, paddingBottom: 120 }}>
       <View>
-        
-         <Text style={{ fontSize: 24, textAlign: "left", color: 'black', marginTop: 20, marginLeft: 20,fontWeight:"bold"}}>
-           Quotes from Mahabharata
-         </Text>
+        <Text
+          style={{
+            fontSize: 24,
+            textAlign: "left",
+            color: "black",
+            marginTop: 20,
+            marginLeft: 20,
+            fontWeight: "bold",
+          }}
+        >
+          Quotes from Mahabharata
+        </Text>
       </View>
 
-      <View style={{ padding:20 }}>
-        <SliderBox images={MahabharataImages}
-        sliderBoxHeight={450}
-        sliderBoxWidth={350}  // Set the height of the slider
-        ImageComponentStyle={{ borderRadius: 15, width: '100%', margin: 0 }}
-        dotColor="#FFEE58"
-        inactiveDotColor="#90A4AE"
-        paginationBoxVerticalPadding={20}
-        autoplay={true}
-        circleLoop
+      <View style={{ padding: 20 }}>
+        <SliderBox
+          images={MahabharataImages}
+          sliderBoxHeight={450}
+          sliderBoxWidth={350} // Set the height of the slider
+          ImageComponentStyle={{ borderRadius: 15, width: "100%", margin: 0 }}
+          dotColor="#FFEE58"
+          inactiveDotColor="#90A4AE"
+          paginationBoxVerticalPadding={20}
+          autoplay={true}
+          circleLoop
         />
       </View>
 
       <View>
-        <Text style={{ fontSize: 24, textAlign: "left", color: 'black', marginLeft: 20,fontWeight:"bold"}}>
+        <Text
+          style={{
+            fontSize: 24,
+            textAlign: "left",
+            color: "black",
+            marginLeft: 20,
+            fontWeight: "bold",
+          }}
+        >
           Quotes from Geeta
         </Text>
       </View>
 
-      
-      <View style={{ padding:20 }}>
-        <SliderBox images={GeetaImages}
-        sliderBoxHeight={450}
-        sliderBoxWidth={350}  // Set the height of the slider
-        ImageComponentStyle={{ borderRadius: 15, width: '100%', margin: 0 }}
-        dotColor="#FFEE58"
-        inactiveDotColor="#90A4AE"
-        paginationBoxVerticalPadding={20}
-        autoplay={true}
-        circleLoop
+      <View style={{ padding: 20 }}>
+        <SliderBox
+          images={GeetaImages}
+          sliderBoxHeight={450}
+          sliderBoxWidth={350} // Set the height of the slider
+          ImageComponentStyle={{ borderRadius: 15, width: "100%", margin: 0 }}
+          dotColor="#FFEE58"
+          inactiveDotColor="#90A4AE"
+          paginationBoxVerticalPadding={20}
+          autoplay={true}
+          circleLoop
         />
       </View>
       <View>
-        <Text style={{ fontSize: 24, textAlign: "left", color: 'black', paddingLeft: 20,fontWeight:"bold" }}>
+        <Text
+          style={{
+            fontSize: 24,
+            textAlign: "left",
+            color: "black",
+            paddingLeft: 20,
+            fontWeight: "bold",
+          }}
+        >
           Quotes from Vedas
         </Text>
       </View>
 
-      <View style={{ padding:20 }}>
-        <SliderBox images={VedasImages}
-        sliderBoxHeight={450}
-        sliderBoxWidth={350} 
-        ImageComponentStyle={{ borderRadius: 15, width: '100%', margin: 0 }}
-        dotColor="#FFEE58"
-        inactiveDotColor="#90A4AE"
-        paginationBoxVerticalPadding={20}
-        autoplay={true}
-        circleLoop
+      <View style={{ padding: 20 }}>
+        <SliderBox
+          images={VedasImages}
+          sliderBoxHeight={450}
+          sliderBoxWidth={350}
+          ImageComponentStyle={{ borderRadius: 15, width: "100%", margin: 0 }}
+          dotColor="#FFEE58"
+          inactiveDotColor="#90A4AE"
+          paginationBoxVerticalPadding={20}
+          autoplay={true}
+          circleLoop
         />
       </View>
-      
-      
     </ScrollView>
   );
 };

60c9eb4cb47a0a4fb499aea6a9a671b047e34774
Vaibhav Rana <vbhvrn@gmail.com>
2024-06-24 18:10:12 +0100

    fix event

--- src/hooks/api/event.js
+++ src/hooks/api/event.js
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 import { fetcher, fetcherWithTimeZone } from "../../helper/fetcher";
 
-const isLocal = true;
+const isLocal = false;
 const HOST = isLocal
   ? "http://localhost:3000"
   : "https://sree-ram-backend.vercel.app";
